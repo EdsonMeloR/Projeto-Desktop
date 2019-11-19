cdedc0a5b69c0a53fc69a9a62443e6f1dd9f99a9
EDSON MELO RODRIGUES DA LUZ <edson.mrluz@senacsp.edu.br>
2019-11-18 22:41:40 -0300

    Adicionando formulario de endereço, adicionando ws Correio

---
+++ Projeto Desktop/Formularios/FrmEnderecos.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Projeto_Desktop.Formularios
+{
+    partial class FrmEnderecos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConsultarCep = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtLogradouro = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtComplemento = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtReferencia = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCep = new System.Windows.Forms.MaskedTextBox();
+            this.cmbClientes = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnCadastrarEndereco = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConsultarCep
+            // 
+            this.btnConsultarCep.Location = new System.Drawing.Point(170, 140);
+            this.btnConsultarCep.Name = "btnConsultarCep";
+            this.btnConsultarCep.Size = new System.Drawing.Size(88, 23);
+            this.btnConsultarCep.TabIndex = 0;
+            this.btnConsultarCep.Text = "Consultar Cep";
+            this.btnConsultarCep.UseVisualStyleBackColor = true;
+            this.btnConsultarCep.Click += new System.EventHandler(this.btnConsultarCep_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id Endereço";
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(12, 89);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 2;
+            // 
+            // txtLogradouro
+            // 
+            this.txtLogradouro.Location = new System.Drawing.Point(12, 192);
+            this.txtLogradouro.Name = "txtLogradouro";
+            this.txtLogradouro.Size = new System.Drawing.Size(224, 20);
+            this.txtLogradouro.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Logradouro";
+            // 
+            // txtComplemento
+            // 
+            this.txtComplemento.Location = new System.Drawing.Point(12, 244);
+            this.txtComplemento.Name = "txtComplemento";
+            this.txtComplemento.Size = new System.Drawing.Size(333, 20);
+            this.txtComplemento.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Complemento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Cep";
+            // 
+            // txtReferencia
+            // 
+            this.txtReferencia.Location = new System.Drawing.Point(12, 283);
+            this.txtReferencia.Multiline = true;
+            this.txtReferencia.Name = "txtReferencia";
+            this.txtReferencia.Size = new System.Drawing.Size(333, 54);
+            this.txtReferencia.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Referencia";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(245, 192);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(242, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Numero";
+            // 
+            // txtCep
+            // 
+            this.txtCep.Location = new System.Drawing.Point(12, 140);
+            this.txtCep.Mask = "00000-000";
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(152, 20);
+            this.txtCep.TabIndex = 13;
+            // 
+            // cmbClientes
+            // 
+            this.cmbClientes.FormattingEnabled = true;
+            this.cmbClientes.Location = new System.Drawing.Point(12, 35);
+            this.cmbClientes.Name = "cmbClientes";
+            this.cmbClientes.Size = new System.Drawing.Size(121, 21);
+            this.cmbClientes.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Clientes";
+            // 
+            // btnCadastrarEndereco
+            // 
+            this.btnCadastrarEndereco.Location = new System.Drawing.Point(12, 343);
+            this.btnCadastrarEndereco.Name = "btnCadastrarEndereco";
+            this.btnCadastrarEndereco.Size = new System.Drawing.Size(121, 23);
+            this.btnCadastrarEndereco.TabIndex = 16;
+            this.btnCadastrarEndereco.Text = "Cadastrar Endereco";
+            this.btnCadastrarEndereco.UseVisualStyleBackColor = true;
+            this.btnCadastrarEndereco.Click += new System.EventHandler(this.btnCadastrarEndereco_Click);
+            // 
+            // FrmEnderecos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 450);
+            this.Controls.Add(this.btnCadastrarEndereco);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cmbClientes);
+            this.Controls.Add(this.txtCep);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtReferencia);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtComplemento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLogradouro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConsultarCep);
+            this.Name = "FrmEnderecos";
+            this.Text = "Enderecos";
+            this.Load += new System.EventHandler(this.FrmEnderecos_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConsultarCep;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtLogradouro;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtComplemento;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtReferencia;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox txtCep;
+        private System.Windows.Forms.ComboBox cmbClientes;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnCadastrarEndereco;
+    }
+}
---
+++ Projeto Desktop/Formularios/FrmEnderecos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Projeto_Desktop.Classes;
+
+namespace Projeto_Desktop.Formularios
+{
+    public partial class FrmEnderecos : Form
+    {
+        public FrmEnderecos()
+        {
+            InitializeComponent();
+        }
+        private void btnConsultarCep_Click(object sender, EventArgs e)
+        {
+            var cep = txtCep.Text.Replace("-","");
+            var ws = new WsCorreios.AtendeClienteClient();
+            var endereco = ws.consultaCEP(txtCep.Text.Trim());
+            txtCep.Text = endereco.cep.ToString();
+            txtLogradouro.Text = endereco.end;
+        }
+        private void FrmEnderecos_Load(object sender, EventArgs e)
+        {
+            Cliente c = new Cliente();
+            foreach(var cli in c.ListarCliente())
+            {
+                cmbClientes.Items.Add(cli.RazaoSocial.ToString());
+                cmbClientes.ValueMember = cli.Id.ToString();
+            }
+        }
+        private void btnCadastrarEndereco_Click(object sender, EventArgs e)
+        {
+            Endereco end = new Endereco();
+            end.InserirEndereco(txtLogradouro.Text, txtCep.Text.Replace("-", ""), txtNumero.Text, txtComplemento.Text, txtReferencia.Text, Convert.ToInt32(cmbClientes.ValueMember));
+            if(end.Id > 0)
+            {
+                MessageBox.Show("Endereço inserido com sucesso");
+                txtId.Text = end.Id.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Falha ao inserir endereço");
+            }
+        }
+    }
+}
